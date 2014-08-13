21bd80c856dabd2e48e135be88d3d3c2ec5f98eb
Sgarcia10 <garciasantiago10@gmail.com>
2014-08-13 00:39:14 -0500

    Primer Commit del proj

---
+++ svnBasico/src/main/java/com/mycompany/svnbasico/src/controller/userController.js
@@ -0,0 +1,37 @@
+    App.Controller.UserController = Backbone.View.extend({
+        el: '#main',
+        initialize: function(options) {
+            this.editTemplate = _.template($('#user').html());
+			this.editTemplate2 = _.template($('#user2').html());
+            var self = this;
+            Backbone.on('user-create', function(params) {
+                self.create(params);
+            });
+            Backbone.on('user-save', function(params) {
+                self.save(params);
+            });
+            Backbone.on('user-cancel', function(params) {
+                self.cancel(params);
+            });
+        },
+        create: function() {
+            this.userModel = new App.Model.UserModel();
+            this._renderEdit();
+        },
+        save:function() { 
+            var model = $('#userForm').serializeObject();
+			this._renderEdit2();
+            //alert('saved model: '+JSON.stringify(model));
+        },
+        cancel: function(){
+            alert('Cancel');
+        },
+        _renderEdit: function() {
+            var self = this;
+            self.$el.html(self.editTemplate({user: self.userModel}));
+        },
+		_renderEdit2: function() {
+            var self = this;
+            self.$el.html(self.editTemplate2({user: self.userModel}));
+        }
+    });
